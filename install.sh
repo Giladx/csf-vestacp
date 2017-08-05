604dfd82c34558e2012f0f905a835954fe62dd76
giladx <gilad.x@gmail.com>
2017-08-05 03:54:36 +0300

    vestacp installation removed from script

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "Let's start..."
 function InstallCSF()
 {
 	echo "Install CSF..."
-	
+
 	cd /usr/src
 	rm -fv csf.tgz
 	wget https://download.configserver.com/csf.tgz
@@ -27,24 +27,18 @@ function InstallCSF()
 # Let's install the Vesta / CSF script
 function InstallVestaCPBashScript()
 {
-	echo "Install VestaCP Script..."
-	
-	cd /tmp
-	wget -O /usr/local/vesta/bin/v-csf http://vestacp.ss88.uk/Install_CSF_on_VestaCP/v-csf.txt
-	chmod 770 /usr/local/vesta/bin/v-csf
-}
 
 # Let's install the CSF Vesta UI!
 function InstallVestaCPFrontEnd()
 {
 	echo "Install VestaCP Front..."
-	
+
 	cd /tmp
 	mkdir /usr/local/vesta/web/list/csf
 	wget http://vestacp.ss88.uk/Install_CSF_on_VestaCP/csf.zip
 	unzip /tmp/csf.zip -d /usr/local/vesta/web/list/
 	rm -f /tmp/csf.zip
-	
+
 	# Update the panel.html file
 	wget -O /usr/local/vesta/web/templates/admin/panel.html http://vestacp.ss88.uk/Install_CSF_on_VestaCP/panel.html
 }
